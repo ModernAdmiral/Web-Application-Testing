2d83717df0a9d4f7ce218328bbaf637fcc895393
Jackson McComas <jackson.mccomas@gmail.com>
2019-10-31 18:09:04 -0500

    converted to class syntax for extra practice

--- testing-react/src/Components/Dashboard.js
+++ testing-react/src/Components/Dashboard.js
@@ -1,29 +1,36 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 import Display from "./Display";
 
-const Dashboard = () => {
-  const [ball, setBall] = useState(0);
-  const [strike, setStrike] = useState(0);
-  return (
-    <>
-      <Display balls={ball} strikes={strike} />
-      <h1>Dashboard</h1>
-      <button
-        onClick={() => {
-          setBall(ball + 1);
-        }}
-      >
-        ball!
-      </button>
-      <button
-        onClick={() => {
-          setStrike(strike + 1);
-        }}
-      >
-        strike!
-      </button>
-    </>
-  );
-};
-
-export default Dashboard;
+export default class Dashboard extends Component {
+  //   const [ball, setBall] = useState(0);
+  //   const [strike, setStrike] = useState(0);
+  constructor() {
+    super();
+    this.state = {
+      balls: 0,
+      strikes: 0
+    };
+  }
+  render() {
+    return (
+      <>
+        <Display balls={this.state.balls} strikes={this.state.strikes} />
+        <h1>Dashboard</h1>
+        <button
+          onClick={() => {
+            this.setState({ balls: this.state.balls + 1 });
+          }}
+        >
+          ball!
+        </button>
+        <button
+          onClick={() => {
+            this.setState({ strikes: this.state.strikes + 1 });
+          }}
+        >
+          strike!
+        </button>
+      </>
+    );
+  }
+}
